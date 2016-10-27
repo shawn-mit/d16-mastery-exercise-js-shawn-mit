7448ab5e395d68debbc6dc38b25c730c2c3e51c4
Shawn M <forgeforza@outlook.com>
2016-10-27 01:34:27 -0500

    intial commit

---
+++ quiz.js
@@ -0,0 +1,46 @@
+//get that tree setup with key value pairs dynamically determined by user input 
+
+var theTree = {
+	height:function () {
+	  document.getElementById ("tall").value
+		}   
+	character: function() {
+		document.getElementById("char").value
+		} 
+	};	
+
+
+//then an array will be constructed which will be used to loop throgh
+// is the user input of height = to the array length? so input = 20 therefore array equals 20 (0-19)?
+// do I just push what the character input is into the array as a string? 
+
+
+//should I create a global varialbe as userInput to hold a function ?
+
+
+function userInput (){
+
+}
+//setup a for loop to iterate through the newly established array
+// and begin looping with a blank space inserted starting at [i = 1]?
+
+
+/*
+
+for (i = 0; i < tree.length ; i++){
+
+	function increase(tree) {
+		tree.val++
+
+	}
+}
+*/
+
+
+
+//  click event for tree button 
+
+//	document.getElementById("treebutton").addEventListener("click", function(){
+	document.getElementById("output").innerHTML = ""
+}statu
+
