00fcdb348bce5dd266302f17200fd5a7773302bb
Shawn M <forgeforza@outlook.com>
2016-10-31 16:13:00 -0500

    second commit

--- quiz.js
+++ quiz.js
@@ -1,59 +1,50 @@
-//get that tree setup with key value pairs dynamically determined by user input 
+//get tree setup with key value pairs dynamically determined by user input 
 
 var theTree = {};
+
 //var x = document.getElementById("myText").value;
 
  function getInput () {
+
 	theTree.height = document.getElementById ("tall").value;
 	theTree.character = document.getElementById("char").value;
 	
-	}
+	};
+
+	if ( theTree.height === "")
+		alert ("Must enter a number!");
+		
+	} 
+	else { 
+		
+		growTree()
+	}; 	
 
 	// create if else for conditions of user interaction
 
-	if ( theTree.height.length   ) {
+function growTree () {
 
-		for (i = 0; i < theTree.height; i++){
+		var treeBranch = "";
 
-		}
+		for (i = 0; i < theTree.height.length; i++){
+			treeBranch +=  insertSpaces;
 
-
-			;
-	}
-
-	else  
-	{
-	  alert("Why you No play the game?");
-
-  /*
-	for ( i = 0; i < theTree.height; i++){
-
-	 theTree = theKeys.push[i];
-
-		};
-
-		*/
-
-console.log(theTree);
-
-//then an array will be constructed which will be used to loop throgh
-// is the user input of height = to the array length? so input = 20 therefore array equals 20 (0-19)?
-// do I just push what the character input is into the array as a string? 
+			}
+		}	 
 
 
-//should I create a global varialbe as userInput to hold a function ?
 
-//setup a for loop to iterate through the newly established array
-// and begin looping with a blank space inserted starting at [i = 1]?
+	  /*space = ""  
+	  loop through giving the array spaces 
 
-
+	  .repeat()  // use for tree . */
 
 
 
 //  click event for tree button
 //treebutton.addEventListener("click",  ); 
 
-//	document.getElementById("treebutton").addEventListener("click", function(){
+//	document.getElementById("treebutton").addEventListener("click", y ) 
 	document.getElementById("output").innerHTML = ""
-}statu
+
 
