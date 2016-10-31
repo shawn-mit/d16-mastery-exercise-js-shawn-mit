9afb16ad8fbb9b7d425858a1fa382dc927e5dc19
Shawn M <forgeforza@outlook.com>
2016-10-31 00:11:35 -0500

    working

--- quiz.js
+++ quiz.js
@@ -1,14 +1,40 @@
 //get that tree setup with key value pairs dynamically determined by user input 
 
-var theTree = {
-	height:function () {
-	  document.getElementById ("tall").value
-		}   
-	character: function() {
-		document.getElementById("char").value
-		} 
-	};	
+var theTree = {};
+//var x = document.getElementById("myText").value;
 
+ function getInput () {
+	theTree.height = document.getElementById ("tall").value;
+	theTree.character = document.getElementById("char").value;
+	
+	}
+
+	// create if else for conditions of user interaction
+
+	if ( theTree.height.length   ) {
+
+		for (i = 0; i < theTree.height; i++){
+
+		}
+
+
+			;
+	}
+
+	else  
+	{
+	  alert("Why you No play the game?");
+
+  /*
+	for ( i = 0; i < theTree.height; i++){
+
+	 theTree = theKeys.push[i];
+
+		};
+
+		*/
+
+console.log(theTree);
 
 //then an array will be constructed which will be used to loop throgh
 // is the user input of height = to the array length? so input = 20 therefore array equals 20 (0-19)?
@@ -17,28 +43,15 @@ var theTree = {
 
 //should I create a global varialbe as userInput to hold a function ?
 
-
-function userInput (){
-
-}
 //setup a for loop to iterate through the newly established array
 // and begin looping with a blank space inserted starting at [i = 1]?
 
 
-/*
-
-for (i = 0; i < tree.length ; i++){
-
-	function increase(tree) {
-		tree.val++
-
-	}
-}
-*/
 
 
 
-//  click event for tree button 
+//  click event for tree button
+//treebutton.addEventListener("click",  ); 
 
 //	document.getElementById("treebutton").addEventListener("click", function(){
 	document.getElementById("output").innerHTML = ""
